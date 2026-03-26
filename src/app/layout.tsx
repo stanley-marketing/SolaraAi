2312275a8401626595a00df485687f6b48706758
Yuval Strutti <yuvals41@gmail.com>
2026-03-26 11:08:43 +0200

    feat: add Google Tag Manager and Mixpanel tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,6 +59,18 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${playfair.variable} ${GeistSans.variable} ${inter.variable}`}>
       <body className="bg-white text-ink-900">
+        <Script id="gtm" strategy="afterInteractive">{`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TJ5RTQ9Z');
+        `}</Script>
+        <Script id="mixpanel" strategy="afterInteractive">{`
+          (function(e,c){if(!c.__SV){var l,h;window.mixpanel=c;c._i=[];c.init=function(q,r,f){function t(d,a){var g=a.split(".");2==g.length&&(d=d[g[0]],a=g[1]);d[a]=function(){d.push([a].concat(Array.prototype.slice.call(arguments,0)))}}var b=c;"undefined"!==typeof f?b=c[f]=[]:f="mixpanel";b.people=b.people||[];b.toString=function(d){var a="mixpanel";"mixpanel"!==f&&(a+="."+f);d||(a+=" (stub)");return a};b.people.toString=function(){return b.toString(1)+".people (stub)"};l="disable time_event track track_pageview track_links track_forms track_with_groups add_group set_group remove_group register register_once alias unregister identify name_tag set_config reset opt_in_tracking opt_out_tracking has_opted_in_tracking has_opted_out_tracking clear_opt_in_out_tracking start_batch_senders start_session_recording stop_session_recording people.set people.set_once people.unset people.increment people.append people.union people.track_charge people.clear_charges people.delete_user people.remove".split(" ");for(h=0;h<l.length;h++)t(b,l[h]);var n="set set_once union unset remove delete".split(" ");b.get_group=function(){function d(p){a[p]=function(){b.push([g,[p].concat(Array.prototype.slice.call(arguments,0))])}}for(var a={},g=["get_group"].concat(Array.prototype.slice.call(arguments,0)),m=0;m<n.length;m++)d(n[m]);return a};c._i.push([q,r,f])};c.__SV=1.2;var k=e.createElement("script");k.type="text/javascript";k.async=!0;k.src="//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";var s=e.getElementsByTagName("script")[0];s.parentNode.insertBefore(k,s)}})(document,window.mixpanel||[]);
+          mixpanel.init('4d4a15b923b6da7abebabea6fb749588',{autocapture:true,record_sessions_percent:100,cookie_domain:".solaraai.com",cross_subdomain_cookie:true,persistence:"localStorage",record_mask_text_selector:'',record_block_selector:''});
+          mixpanel.track("Marketing Page Viewed",{page:window.location.pathname,title:document.title,referrer:document.referrer});
+        `}</Script>
         <Script id="meta-pixel" strategy="afterInteractive">{`
           !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
           n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
@@ -69,6 +81,13 @@ export default function RootLayout({
           fbq('track','PageView');
         `}</Script>
         <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TJ5RTQ9Z"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="GTM"
+          />
           <img
             height="1"
             width="1"
